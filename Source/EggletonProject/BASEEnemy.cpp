5b8e167f8472f58bf9ed2c794b1ee612cc5346b2
Lachlan Brown <lachlan.brown8@live.com.au>
2014-11-06 21:00:06 +1100

    no message

---
+++ Source/EggletonProject/BASEEnemy.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EggletonProject.h"
+#include "BASEEnemy.h"
+
+
+ABASEEnemy::ABASEEnemy(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/EggletonProject/BASEEnemy.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "BASEEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EGGLETONPROJECT_API ABASEEnemy : public AActor
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
